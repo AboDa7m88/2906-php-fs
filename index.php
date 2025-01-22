c2b63c3f05773a1e9ad28c67983d45256adc22a2
Abdelrahman <bm1208347@gmail.com>
2025-01-22 20:22:13 +0200

    Sum function was added

--- index.php
+++ index.php
@@ -12,10 +12,19 @@ greet('Abdelrahman');
 // Age Function
 function Age ($name, $age){
     echo "$name is $age years old";
+    echo '<hr>';
 }
 
 Age('Abdelrahman','22');
 
 
+// sum of two num.s
+function sum ($n1, $n2) {
+    $n3 = $n1 + $n2;
+    echo "Sum is:  $n3";
+    echo '<hr>';
+}
+
+sum('3' ,'5');
 
 ?>
